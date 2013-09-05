17478d66ab687bd46095342b1aeaace3045dea96
taintedromance9021@gmail.com <taintedromance9021@gmail.com@087010b0-2ed2-c774-f015-95aab1926f92>
2013-09-05 21:40:34 +0000

    This method hadles retriving pitstop data and other pitstop actions.

---
+++ RoadTrip/src/nz/ac/waikato/cs/roadtrip/Pitstop.java
@@ -0,0 +1,9 @@
+package nz.ac.waikato.cs.roadtrip;
+
+public class Pitstop {
+
+	// This method takes a pitstop and returns more details about it. (Used when a user selects a pitstop)
+	
+	// This method returns all pitstops, given a trip. (Maybe account for a single location)
+	
+}
