9a9983acb72a702eee115564482454cd64a76e89
robertmurrayburke <robertmurrayburke@gmail.com>
2013-09-09 11:56:11 +0000

    Began development for passing a Trip object between the maps page and trip page

--- RoadTrip/src/nz/ac/waikato/cs/roadtrip/TripPage.java
+++ RoadTrip/src/nz/ac/waikato/cs/roadtrip/TripPage.java
@@ -1,19 +1,46 @@
 package nz.ac.waikato.cs.roadtrip;
 
+import nz.ac.waikato.cs.roadtrip.helpers.MessageBoxHelper;
+import nz.ac.waikato.cs.roadtrip.models.SerializableTrip;
+import nz.ac.waikato.cs.roadtrip.models.Trip;
+import nz.ac.waikato.cs.roadtrip.models.TripCategories;
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.Spinner;
 import android.support.v4.app.NavUtils;
 
 public class TripPage extends Activity {
 
+	SerializableTrip newTrip;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_trip_page);
 		// Show the Up button in the action bar.
 		setupActionBar();
+		
+		Bundle b = this.getIntent().getExtras();
+		if(b!=null)
+		    newTrip = (SerializableTrip) b.getSerializable("trip");
+		
+		setOriginEditText();
+	}
+
+	private void setOriginEditText() {
+		// TODO Auto-generated method stub
+		if(newTrip.hasStart()){	
+			EditText origin = (EditText)findViewById(R.id.editTextOrigin);
+			if(origin != null )
+				origin.setText("Current Location");
+		}
 	}
 
 	/**
@@ -48,5 +75,69 @@ public class TripPage extends Activity {
 		}
 		return super.onOptionsItemSelected(item);
 	}
+	
+	public void buttonGoClick(View view) {
+		if(allControlValuesAreNotNullAndValid()){
+			EditText origin = (EditText)findViewById(R.id.editTextOrigin);
+			EditText destination = (EditText)findViewById(R.id.editTextDestination);
+			
+			CheckBox cbAcc = (CheckBox)findViewById(R.id.cbAccomodation);
+			CheckBox cbEnt = (CheckBox)findViewById(R.id.cbEntertainment);
+			CheckBox cbFood = (CheckBox)findViewById(R.id.cbFood);
+			CheckBox cbLand = (CheckBox)findViewById(R.id.cbLandmarks);
+			CheckBox cbWalks = (CheckBox)findViewById(R.id.cbWalks);
+			
+			Spinner spin = (Spinner)findViewById(R.id.spinnerRadius);
+			
+			//newTrip.tripCategories = new TripCategories(cbAcc.isChecked(), cbEnt.isChecked(), cbFood.isChecked(), cbLand.isChecked(), cbWalks.isChecked());
+			newTrip.setEnd(destination.getText().toString());
+			newTrip.setRaduis(getRadiusFromId(spin.getSelectedItemId()));
+			
+			Intent result = new Intent();
+			
+			if(!origin.getText().toString().equalsIgnoreCase("Current Location")){
+				newTrip.setStart(origin.getText().toString());
+			}
+			
+			result.putExtra("trip", newTrip);
+			setResult(RESULT_OK, result);
+		}
+		else
+			MessageBoxHelper.showMessageBox(this, "Incorrect Inputs", "One or more of the input are incorrect");
+	}
 
+	private double getRadiusFromId(long selectedItemId) {
+		switch ((int) selectedItemId){
+		
+		case 1: selectedItemId = 0;
+			return 500;
+		case 2: selectedItemId = 1;
+		return 1000;
+		case 3: selectedItemId = 2;
+		return 1500;
+		case 4: selectedItemId = 3;
+		return 2000;
+		case 5: selectedItemId = 4;
+		return 2500;
+		case 6: selectedItemId = 5;
+		return 3000;
+		case 7: selectedItemId = 6;
+		return 3500;
+		case 8: selectedItemId = 7;
+		return 4000;
+		case 9: selectedItemId = 8;
+		return 4500;
+		case 10: selectedItemId = 9;
+		return 5000;
+		case 11: selectedItemId = 10;
+		return 5500;
+		}
+		
+		return -1;
+	}
+
+	private boolean allControlValuesAreNotNullAndValid() {
+		// TODO Auto-generated method stub
+		return true;
+	}
 }
---
+++ RoadTrip/src/nz/ac/waikato/cs/roadtrip/models/SerializableTrip.java
@@ -0,0 +1,50 @@
+package nz.ac.waikato.cs.roadtrip.models;
+
+import java.io.Serializable;
+
+//used to pass between maps page and trip page
+	public class SerializableTrip implements Serializable{
+
+		private static final long serialVersionUID = 1L;
+		private String start;
+		private String end;
+		private double radius;
+		
+		public SerializableTrip(String start, String end, double radius){
+			this.start = start;
+			this.end = end;
+			this.radius = radius;
+		}
+		
+		public boolean hasStart(){
+			return start != null;
+		}
+
+		public void setEnd(String string) {
+			end = string;
+			
+		}
+
+		public void setRaduis(double radiusFromId) {
+			radius = radiusFromId;
+			
+		}
+
+		public void setStart(String string) {
+			start = string;
+			
+		}
+
+		public String getStart() {
+			return start;
+		}
+
+		public String getEnd() {
+			return end;
+		}
+
+		public double getRadius() {
+			// TODO Auto-generated method stub
+			return radius;
+		}
+	}
